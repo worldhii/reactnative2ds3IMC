64c7dddac5e3125b0184c72d326b18d15f127bb1
jussimar <jussimar@jussimarleal.com.br>
2025-04-25 22:40:50 -0300

    Aula 25-04-2025

--- App.tsx
+++ App.tsx
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  function calcularImc(){
+    alert("Calculo do IMC");
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}> IMC </Text>
+      
+      <View style={styles.bloco}>
+          <Text style={styles.label}>Peso</Text>
+          <TextInput 
+            style={styles.input}
+            keyboardType='numeric'
+          />
+      </View>
+      <View style={styles.bloco}>
+          <Text style={styles.label} >Altura</Text>
+          <TextInput 
+            style={styles.input}
+            keyboardType='numeric'
+            
+          />
+      </View>
+      <View style={styles.bloco}>
+          <TouchableOpacity style={styles.btn} onPress={calcularImc} >
+              <Text style={styles.btnTxt}>Calcular</Text>
+          </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +38,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ccc',
   },
+  titulo:{
+    textAlign:'center',
+    fontSize:30,
+    marginTop:80
+  },
+  bloco:{
+    width:'100%',
+    marginTop:20
+  },
+  label:{
+    width:'80%',
+    fontSize:20,
+    marginLeft:'10%'
+  },
+  input:{
+    width:'80%',
+    fontSize:20,
+    marginLeft:'10%',
+    borderWidth:1,
+    borderRadius:10
+  },
+  btn:{
+    width:'80%',
+    marginLeft:'10%',
+    backgroundColor:'#000'
+  },
+  btnTxt:{
+    fontSize:30,
+    color:"#FFF",
+    textAlign:'center'
+  }
 });
